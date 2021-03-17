af0f1ebe3fcaf28189e17b8fc50647c285f3535a
gorlemZ <andrea.alice.gorletta@gmail.com>
2021-03-17 12:59:10 +0100

    ac test

---
+++ src/main/java/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+
+    /**
+     * creare una funzione con firma:
+     *    public static double media(....){}
+     * con parametri a piacimento, che calcola la media di una sequenza numerica.
+     */
+
+
+    public static void main(String[] args) {
+
+
+        /**
+         * istanziare 10 veicoli a scelta tra Bicicletta,
+         * Triciclo o Automobile, con velocità e caratteristiche tutte diverse tra loro.
+         */
+
+        /**
+         * creare un ARRAY (non una lista) che contenga gli oggetti creati.
+         */
+
+        System.out.println("I seguenti veicoli vanno in media più veloci degli altri");
+
+        /**
+         *  Stamapre, attraverso un ciclo che analizza TUTTI E DIECI gli oggetti,
+         *  i dettagli dei soli veicoli con velocità superiorie
+         *  alla media calcolata dalla funzione media().
+         */
+
+        System.out.println("I seguenti veicoli invece hanno un numero di ruote superiore alla media");
+
+        /**
+         *  Stamapre, attraverso un ciclo che analizza TUTTI E DIECI gli oggetti,
+         *  i dettagli dei soli veicoli con numero di ruote superiore
+         *  alla media calcolata dalla funzione media().
+         */
+
+        System.out.println("il terzo elemento della lista è: ");
+        /**
+         * stampare i dettagli del terzo elemento dell'array.
+         */
+
+    }
+
+
+}
+
+

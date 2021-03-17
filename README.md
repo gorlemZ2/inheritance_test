67c38c930614adf3102cb1fad49ec3c5179aead9
gorlemZ <andrea.alice.gorletta@gmail.com>
2021-03-17 13:09:23 +0100

    simple test

--- README.md
+++ README.md
@@ -1,18 +1,11 @@
 ## Esercizio con valutazione, per AL
 
 ### ESERCIZIO 0    
-> Creare una classe Veicolo. Tutti i veicoli hanno una velocità e un numero finito di ruote.
-> La classe deve mettere a disposizione dei metodi get adeguati, tra cui il metodo getDettagli,
-> che stampa la descrizione del veicolo. 
+> Creare una classe Veicolo. 
+> Il costruttore della classe prende in input due numeri:
+> la velocità e il numero di ruote. 
+> La classe mette a disposizione due getter e un metodo dettagli() che stampa 
+> a video una frase che descrive il Veicolo.
 
 ### ESERCIZIO 1
-> Creare Le seguenti sottoclassi di veicolo: Motocicletta, Automobile, Triciclo. 
-> Una motocicletta può andare al massimo a 150 km/h e può essere bianca, verde o blu. 
-> Una Automobile ha una velocità massima di 130 km/h e può avere 3 o 5 portiere.
-> Un Triciclo è lento, va al massimo a 5 km/h.
->
-> I costruttori di Bicicletta, Automobile e Triciclo prendono come parametri solo la velocità e la loro eventuale caratteristica unica (colore, portiere).
-> Il metodo getDettagli() deve essere arricchito con un override in tutte le sottoclassi di veicolo.
-
-### ESERCIZIO 2
 > Completare il Main.
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,37 +11,29 @@ public class Main {
 
 
         /**
-         * istanziare 10 veicoli a scelta tra Bicicletta,
-         * Triciclo o Automobile, con velocità e caratteristiche tutte diverse tra loro.
+         * istanziare 10 veicoli con diverse velocità e diverso numero di ruote (max 4 LOL)
          */
 
         /**
-         * creare un ARRAY (non una lista) che contenga gli oggetti creati.
+         * creare una lista o un array che contenga gli oggetti creati.
          */
 
         System.out.println("I seguenti veicoli vanno in media più veloci degli altri");
 
         /**
-         *  Stamapre, attraverso un ciclo che analizza TUTTI E DIECI gli oggetti,
+         *  Stamapre, attraverso un ciclo FOR che analizza TUTTI E DIECI gli oggetti,
          *  i dettagli dei soli veicoli con velocità superiorie
          *  alla media calcolata dalla funzione media().
          */
 
-        System.out.println("I seguenti veicoli invece hanno un numero di ruote superiore alla media");
+        System.out.println("I seguenti veicoli invece hanno tutti 4 ruote");
 
         /**
-         *  Stamapre, attraverso un ciclo che analizza TUTTI E DIECI gli oggetti,
-         *  i dettagli dei soli veicoli con numero di ruote superiore
-         *  alla media calcolata dalla funzione media().
-         */
-
-        System.out.println("il terzo elemento della lista è: ");
-        /**
-         * stampare i dettagli del terzo elemento dell'array.
+         *  Stamapre, attraverso un ciclo WHILE che analizza TUTTI E DIECI gli oggetti,
+         *  i dettagli dei soli veicoli con 4 ruote.
          */
 
     }
-
 
 }
 

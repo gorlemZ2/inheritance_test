cea2b0bd7ec4dea9d97f73d614accc5d2d13d023
gorlemZ <andrea.alice.gorletta@gmail.com>
2021-03-17 12:53:58 +0100

    esercizio Lepori

---
+++ src/main/java/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+
+    /**
+     * creare una funzione con firma:
+     *    public static double max(....){}
+     * con parametri a piacimento, che calcola il massimo di una sequenza numerica.
+     */
+
+
+    public static void main(String[] args) {
+
+
+        /**
+         * istanziare 10 veicoli a scelta tra Bicicletta,
+         * Triciclo o Automobile, con velocità e caratteristiche tutte diverse tra loro.
+         */
+
+        /**
+         * creare un ARRAY (non una lista) che contenga gli oggetti creati.
+         */
+
+        System.out.println("Il veicolo che va più veloce è: ");
+
+        /**
+         *  Stamapre, attraverso un ciclo che analizza TUTTI E DIECI gli oggetti,
+         *  i dettagli del solo veicolo più veloce
+         *  di tutti gli altri, trovato grazie alla funzione max().
+         */
+
+        System.out.println("I seguenti veicoli invece hanno tutti 4 ruote: ");
+
+        /**
+         *  Stamapre, attraverso un ciclo che analizza TUTTI E DIECI gli oggetti,
+         *  i dettagli dei soli veicoli con numero di ruote == 4.
+         */
+
+        System.out.println("il quinto elemento della lista è: ");
+        /**
+         * stampare i dettagli del quinto elemento dell'array.
+         */
+
+    }
+
+
+}
+
+

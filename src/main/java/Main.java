3eda27efbb58df582a200b6ce04b05832d41e80d
Luca Argentieri <Lucaargentieri-98@hotmail.it>
2021-03-22 09:52:45 +0000

    update compito

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class Main {
 
     /**
@@ -5,20 +6,54 @@ public class Main {
      *    public static double media(....){}
      * con parametri a piacimento, che calcola la media di una sequenza numerica.
      */
+    
+    public static double media(int[] macchine) {
+        int somma = 0;
+        for (int i = 0; i < macchine.length; i++)
+        somma += macchine[i];
+        double media = ((double) somma) / macchine.length;
+        System.out.println(media);
+        return media;
+    }
+
 
 
     public static void main(String[] args) {
 
-
         /**
          * istanziare 10 veicoli con diverse velocità e diverso numero di ruote (max 4 LOL)
          */
+        Veicle macchina = new Veicle(50, 4, "ford");
+        Veicle macchina2 = new Veicle(30, 3, "mini");
+        Veicle macchina3 = new Veicle(20, 3, "ferrari");
+        Veicle macchina4 = new Veicle(10, 4, "lamborghini");
+        Veicle macchina5 = new Veicle(50, 4, "panda");
+        Veicle macchina6 = new Veicle(60, 4, "toyota");
+        Veicle macchina7 = new Veicle(70, 4, "audi");
+        Veicle macchina8 = new Veicle(80, 4, "peugeot");
+        Veicle macchina9 = new Veicle(50, 3, "mini 2");
+        Veicle macchina10 = new Veicle(20, 3, "ford 2");
 
+       
         /**
          * creare una lista o un array che contenga gli oggetti creati.
          */
 
-        System.out.println("I seguenti veicoli vanno in media più veloci degli altri");
+            ArrayList<Veicle> macchine = new ArrayList<Veicle>();
+            macchine.add(macchina);
+            macchine.add(macchina2);
+            macchine.add(macchina3);
+            macchine.add(macchina4);
+            macchine.add(macchina5);
+            macchine.add(macchina6);
+            macchine.add(macchina7);
+            macchine.add(macchina8);
+            macchine.add(macchina9);
+            macchine.add(macchina10);
+
+            // System.out.println(macchine);
+
+            int listaVelocita[] = new int [10];
 
         /**
          *  Stamapre, attraverso un ciclo FOR che analizza TUTTI E DIECI gli oggetti,
@@ -26,12 +61,36 @@ public class Main {
          *  alla media calcolata dalla funzione media().
          */
 
-        System.out.println("I seguenti veicoli invece hanno tutti 4 ruote");
+        for(int i = 0; i < macchine.size(); i++){
+           
+            // prendere velocità di tutti
+            listaVelocita[i] = macchine.get(i).velocita;
+
+            // Pushare dentro lista velocità e fare la media
+        };
+
+        double avg = media(listaVelocita);
+
+        for(int i = 0; i < macchine.size(); i++){
+           
+            if(macchine.get(i).velocita > avg){
+                System.out.println("Questa macchina è una " + macchine.get(i).nome + " ed ha " + macchine.get(i).ruote + " ruote" );
+            }
+        }
+
 
         /**
          *  Stamapre, attraverso un ciclo WHILE che analizza TUTTI E DIECI gli oggetti,
          *  i dettagli dei soli veicoli con 4 ruote.
          */
+
+        int i = 0;
+        while(i < macchine.size()){
+            if(macchine.get(i).ruote == 4){
+                System.out.println(macchine.get(i).nome + " ha 4 ruote");
+            }
+            i++;
+        }
 
     }
 
---
+++ src/main/java/Veicle.java
@@ -0,0 +1,12 @@
+public class Veicle{
+
+    public int velocita;
+    public int ruote;
+    public String nome;
+
+    public Veicle(int velocita, int ruote, String nome){
+        this.velocita = velocita;
+        this.ruote = ruote;
+        this.nome = nome;
+    }
+}
